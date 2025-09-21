4b6e4ad79927f2bc3fa215de870c9486f6393c52
Srivastava Mohit <mshrivastava43082@gmail.com>
2025-09-21 22:45:20 +0530

    just saved something

--- arrays/CountFrequency.java
+++ arrays/CountFrequency.java
@@ -1,4 +1,5 @@
 package arrays;
+
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -9,13 +10,14 @@ public class CountFrequency {
     public static void solution(String str) {
         HashMap<Character, Integer> map = new LinkedHashMap<>();
         for (char ch : str.toCharArray()) {
-            map.put(ch, map.getOrDefault(ch, 0)+1);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
         }
-        for(Map.Entry<Character, Integer> entrySet : map.entrySet()){
+        for (Map.Entry<Character, Integer> entrySet : map.entrySet()) {
             System.out.println(entrySet);
         }
         // System.out.println(map);
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String name = sc.nextLine();
--- arrays/DefusetheBomb.java
+++ arrays/DefusetheBomb.java
@@ -17,9 +17,9 @@ public class DefusetheBomb {
                 arr[i] = sum;
             }
         } else {
-            int kk = -k; 
+            int kk = -k;
             for (int i = 0; i < code.length; i++) {
-                int sum = 0; 
+                int sum = 0;
                 for (int t = 1; t <= kk; t++) {
                     int idx = i - t;
                     idx = ((idx % code.length) + code.length) % code.length;
@@ -33,7 +33,7 @@ public class DefusetheBomb {
 
     public static void main(String[] args) {
         // int code[] = { 5, 7, 1, 4 }, k = 3;
-        int code[] = {2,4,9,3}, k = -2;
+        int code[] = { 2, 4, 9, 3 }, k = -2;
 
         for (int i : decrypt(code, k)) {
             System.out.println(i + " ");
